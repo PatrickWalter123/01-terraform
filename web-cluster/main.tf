881d1c9ed10af850498b16601a2ace016c7cd7f2
PatrickWalter123 <a01053917426@gmail.com>
2022-12-27 16:23:09 +0900

    20221227

--- web-cluster/main.tf
+++ web-cluster/main.tf
@@ -19,18 +19,13 @@ provider "aws" {
 
 
 # 시작 구성 설정
-resource "aws_launch_configuration" "example" {
+resource "aws_launch_template" "example" {
   image_id        = "ami-06eea3cd85e2db8ce"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
   key_name = "aws16-key"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = "${base64encode(data.template_file.web_output.rendered)}"
 
   lifecycle {
     create_before_destroy = true
@@ -41,11 +36,16 @@ resource "aws_launch_configuration" "example" {
 
 # 오토스케일링 그룹
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
-
+  availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
+  
+  desired_capacity = 1
   min_size = 1
   max_size = 2
+
+  launch_template {
+    id = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
@@ -53,6 +53,65 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
+
+
+# 로드 밸런서 대상 그룹 구성
+resource "aws_lb" "example" {
+  name = "aws16-terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+  # 기본값으로 단순한 404 페이지 오류를 반환한다.
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+    content_type = "text/plain"
+    message_body = "404: page not found"
+    status_code = 404
+    }
+  }
+}
+
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+    condition {
+      path_pattern {
+      values = ["*"]
+    }  
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+
+
+resource "aws_lb_target_group" "asg" {
+  name = "aws16-terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
 
 
 # 보안 그룹 설정
@@ -63,6 +122,25 @@ resource "aws_security_group" "instance" {
     from_port   = var.server_port # 출발 포트
     to_port     = var.server_port # 도착 포트
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "alb" {
+  name = "aws16-terraform-example-alb"
+  # 인바운드 HTTP 트래픽 허용
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # 모든 아웃바운드 트래픽 허용
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -79,5 +157,13 @@ data "aws_subnets" "default" {
   filter {
     name = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+
+
+data "template_file" "web_output" {
+  template = file("${path.module}/web.sh")
+    vars = {
+    server_port = "${var.server_port}"
   }
 }
--- web-cluster/vpc/main.tf
+++ web-cluster/vpc/main.tf
@@ -7,11 +7,45 @@ data "aws_vpc" "default" {
   default = true
 }
 
+/*
+data "aws_vpc" "project-vpc" {
+  id = "<vpc id>"
+}
+
+
+data "aws_vpc" "project-vpc" {
+  filter {
+    name = "tag:Name"
+    value = ["Project-VPC"]
+  }
+}
+*/
+
+data "aws_subnets" "example" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+data "aws_subnet" "example" {
+  for_each = toset(data.aws_subnets.example.ids)
+  id = each.value
+}
+
 
 output "vpc-id" {
   value = data.aws_vpc.default.id
 }
 
+
+output "subnet_cidr_block" {
+  value = [for  s in data.aws_subnet.example : s.cidr_block]
+}
+
+
 output "vpc-cidr_block" {
   value = data.aws_vpc.default.cidr_block
 }
+
